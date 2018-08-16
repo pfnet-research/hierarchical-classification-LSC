fbcd89b15bda94e796370b853d8ab110626c6d43
Takuo Kaneko <bomac1@i18-mac-11.local>
2018-08-16 19:39:00 +0900

    add train_cifar10

---
+++ image_classification/train.py
@@ -0,0 +1,21 @@
+import numpy as np
+import chainer
+import chainer.links as L
+import chainer.functions as F
+from chainer import training
+from chainer.training import extensions
+from chainer.backends import cuda
+import random
+import argparse
+import chainer
+import models
+
+
+def main():
+    model = models.ResNet
+
+
+
+
+if __name__ == '__main__':
+    main()
